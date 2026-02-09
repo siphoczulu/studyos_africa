a69643858ed5a9fdf7ce8759f462fbf5b207348a
siphoczulu <156943291+siphoczulu@users.noreply.github.com>
2026-02-09 22:17:02 +0300

    Add minimal app scaffold and courses screen

---
+++ lib/features/courses/ui/courses_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CoursesScreen extends StatelessWidget {
+  const CoursesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Courses'),
+      ),
+      body: const Center(
+        child: Text('Courses screen (DB next)'),
+      ),
+    );
+  }
+}
